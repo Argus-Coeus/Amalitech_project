c74502cb4e0eb43493ccd311741112b3c61274d6
Richard Arthur <richard.arthur003@stu.ucc.edu.gh>
2024-05-23 08:38:51 -0400

    Amalitech project

--- api/serializers.py
+++ api/serializers.py
@@ -1,8 +1,16 @@
 from rest_framework import serializers
 from .models import Video
-
+from django.contrib.auth.models import User
+from .models import Video
 
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         fields = ('id','author','title', 'description', 'Video_file', 'thumbnail', 'date_posted',)
         model = Video
+
+class UserSerializer(serializers.ModelSerializer):
+    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Video.objects.all())
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'snippets']
