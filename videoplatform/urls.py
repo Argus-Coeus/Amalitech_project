22178897cca8a82af921416a51b8934726cf3851
Richard Arthur <richard.arthur003@stu.ucc.edu.gh>
2024-06-23 13:48:13 -0400

    Changes

--- videoplatform/urls.py
+++ videoplatform/urls.py
@@ -1,14 +1,17 @@
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path,include,re_path
 from rest_framework.documentation import include_docs_urls
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.static import serve
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/',include('api.urls')),
     path('auth/', include('auths.urls')),
     path('', include('accounts.urls')),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root':settings.STATIC_ROOT}),
     path('api/docs/', include_docs_urls(title='Video API'),name= "docs"),
     path('api/password_reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),
 ] 
