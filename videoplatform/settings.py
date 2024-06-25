19dab8cd7ba9592c67b326a832f13a251882b3cc
Richard Arthur <richard.arthur003@stu.ucc.edu.gh>
2024-06-25 05:19:26 -0400

    Changes

--- videoplatform/settings.py
+++ videoplatform/settings.py
@@ -30,7 +30,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'api',
     'rest_framework',
-    'accounts',delete my project
+    'accounts',
     'auths',
     'rest_framework_simplejwt.token_blacklist',
     'crispy_forms',
