458870cce7ec3429dea0f60a0fb43acbacb72dac
Richard Arthur <richard.arthur003@stu.ucc.edu.gh>
2024-06-26 12:59:00 -0400

    changes

--- auths/serializers.py
+++ auths/serializers.py
@@ -15,7 +15,7 @@ from django.contrib.auth import authenticate
 from .utils import send_verification_email
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
-    # username_field = get_user_model().EMAIL_FIELD   
+    email_field = get_user_model().EMAIL_FIELD   
 
     @classmethod
     def get_token(cls, user):
@@ -24,8 +24,8 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
         token['email'] = user.email
         return token
 
-    def validate(self, attrs):
-        email = attrs.get('email')
+    def validate(self, attrs,email_field):
+        email = attrs.get(email_field)
         password = attrs.get('password')
 
         # Authenticate user based on email and password
